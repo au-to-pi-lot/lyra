bb676ee5278dc490fbaa0310a0e46d8c676e6981
AUTOPILOT <zuthayis@gmail.com>
2025-10-14 04:26:31 -0500

    Document core struct types

--- src/types/symbol.c
+++ src/types/symbol.c
@@ -1,6 +1,10 @@
 #include "symbol.h"
 #include "../gc.h"
 
+/// @brief Allocate a new symbol.
+/// @param gc The interpreter's garbage collector.
+/// @param value The symbol's name.
+/// @return Symbol Value
 Value *make_symbol(GC *gc, UT_string *value) {
     Value *result = gc_alloc_value(gc, SYMBOL, (ValueData){.as_string = value});
     return result;
--- src/types/value.h
+++ src/types/value.h
@@ -11,6 +11,7 @@ typedef struct Variable Variable;
 typedef struct Derived Derived;
 typedef struct Callable Callable;
 
+
 typedef enum ValueType {
     CONS,
     INT,
@@ -55,31 +56,58 @@ typedef struct GC {
     size_t max_objects;  // trigger GC when we hit this
 } GC;
 
-
+/// @struct Cons
+/// @brief Cons is a single link of an immutable List. 
+/// 
+/// Implemented here in Lisp tradition, with the convention that
+/// `car` point toward the head item of the list, while `cdr` point toward the next
+/// cons cell in the list. This is terminated when `cdr` points to nil.
 struct Cons {
+    /// @brief Pointer to head of list
     Value *car;
+    /// @brief Pointer to tail of list
     Value *cdr;
 };
 
+/// @struct Variable
+/// @brief Variable is an item in a Closure's hash table.
 struct Variable {
+    /// @brief The variable's name. 
     UT_string *key;
+    /// @brief The variable's value
     Value *value;
+    /// @brief Mandatory struct member for uthash items
     UT_hash_handle hh;
 };
 
+/// @struct Closure
+/// @brief Storage mechanism for variable definitions.
+///
+/// Each Function call creates a Closure capturing the definitions inside the function.
+/// The closures form a linked list back to the global closure with lexical binding semantics.
 struct Closure {
     Variable *defs;
     Closure *parent;
 };
 
+/// @brief C lang definition of a function or macro. 
+///
+/// @param gc The interpreter's garbage collector
+/// @param closure The closure in which it is evaluated
+/// @param args The arguments provided when the intrinsic was called
 typedef Value *(*Intrinsic)(GC *gc, Closure *closure, Value *args);
 
+/// @brief A Lyra function or macro.
 struct Derived {
+    /// @brief The function's environment of definition. This is used to implement lexical variable binding.
     Closure *closure;
+    /// @brief A List of parameter tokens.
     Value *params;
+    /// @brief S-expression function definition.
     Value *definition;
 };
 
+/// @brief Discriminated union over CallableType.
 struct Callable {
     CallableType type;
     union {
@@ -88,6 +116,7 @@ struct Callable {
     } data;
 };
 
+/// @brief Union accessors for Value.
 typedef union ValueData {
     Cons *as_cons;
     int as_int;
@@ -99,6 +128,7 @@ typedef union ValueData {
     Callable *as_macro;
 } ValueData;
 
+/// @brief 
 struct Value {
     ValueType type;
     ValueData data;
